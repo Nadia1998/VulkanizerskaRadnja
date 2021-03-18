61b0c8800e06471a1e4027070042f1c90805be5b
Nadia1998 <nadjacoguric998@gmail.com>
2021-03-18 14:34:40 +0100

    Uradjena dokumentacija interfejsa Radnja i klase VulkanizerskaRadnja.

--- src/auto_radnja/VulkanizerskaRadnja.java
+++ src/auto_radnja/VulkanizerskaRadnja.java
@@ -4,8 +4,19 @@ import java.util.LinkedList;
 
 import auto_radnja.gume.AutoGuma;
 
+/**
+ * 
+ * Ova klasa implementira interfejs radnja i predstavlja implementaciju preko
+ * liste.
+ * 
+ * @author Nadia
+ *
+ */
 public class VulkanizerskaRadnja implements Radnja {
 
+	/**
+	 * Lista sa svim gumama u radnji.
+	 */
 	private LinkedList<AutoGuma> gume = new LinkedList<AutoGuma>();
 
 	@Override
