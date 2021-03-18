b57312c85d62ca0cb272827741b452cddbaca485
Nadia1998 <nadjacoguric998@gmail.com>
2021-03-18 14:35:03 +0100

    Odradjena dokumentacija javadoc.

--- src/auto_radnja/Radnja.java
+++ src/auto_radnja/Radnja.java
@@ -3,10 +3,31 @@ package auto_radnja;
 import java.util.LinkedList;
 
 import auto_radnja.gume.AutoGuma;
-
+/**
+ * Interfejs koji implementira radnju
+ * 
+ * @author Nadia
+ *
+ */
 public interface Radnja {
-	
+	/**
+	 * Dodaje novu gumu u radnju.
+	 * 
+	 * @param a guma koja se dodaje kao instanca klase AutoGuma
+	 * 
+	 * @throws java.lang.NullPointerException ako je uneta guma null.
+	 * 
+	 * @throws java.lang.RuntimeException ako u listi guma vec postoji ova guma.
+	 * 
+	 */
 	void dodajGumu(AutoGuma a);
-
+    /**
+     * Pronalazi gumu u listi guma.
+     * 
+     * @param markaModel marka i model gume kao String koja se pronalazi u listi guma.
+     * 
+     * @return vraca gumu koju je pronasla metoda kao instancu klase AutoGuma.
+     * 
+     */
 	LinkedList<AutoGuma> pronadjiGumu(String markaModel);
 }
